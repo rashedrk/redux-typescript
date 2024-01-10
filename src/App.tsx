5e0415ce2bb577fe85a68f846cc8d192056614f3
rashedrk <rashedrk001@gmail.com>
2024-01-10 21:26:04 +0600

    initialize store and slice

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@
 function App() {
 
   return (
-    <>
-    <h1>This is react redux app</h1>
-    </>
+    <div className="h-screen w-full flex justify-center items-center">
+    <div className="flex border border-purple-200 rounded-md bg-slate-50 p-10">
+      <button className="px-2 py-3 bg-green-500 rounded-md text-white font-bold">Increment</button>
+      <h1 className="mx-10 text-2xl">0</h1>
+      <button className="px-2 py-3 bg-red-500 rounded-md text-white font-bold">Decrement</button>
+    </div>
+    </div>
   )
 }
 
